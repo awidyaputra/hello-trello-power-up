9fbbc1bf779e1c7128efee5ed095db25f54921ee
Aulia Widyaputra <aulia.widyaputra@gmail.com>
2020-12-13 11:04:46 +0700

    Hit backend and return result in badge

--- src/js/connector.js
+++ src/js/connector.js
@@ -3,6 +3,27 @@ console.log("Hello World!");
 window.TrelloPowerUp.initialize({
   'card-badges': function (t, opts) {
     // return an array of card badges for the given card
-    return [];
+    return t.card()
+      .then(function (card) {
+        return fetch('https://hidden-dawn-50339.herokuapp.com/echo', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            foo: 'bar'
+          })
+        })
+          .then(function (response) {
+            return response.json()
+          })
+          .then(function (backendData) {
+            return [
+              {
+                text: backendData.foo
+              }
+            ]
+          })
+      })
   }
 })
